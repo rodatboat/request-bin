b66c6c4b1503507294dd9a96467481e8294022e8
Ronaldo Suarez <ronaldo3055@gmail.com>
2023-05-04 13:33:01 -0400

    Adding favicon and seo stuff

--- renderer/_default.page.server.jsx
+++ renderer/_default.page.server.jsx
@@ -26,10 +26,22 @@ async function render(pageContext) {
   const documentHtml = escapeInject`<!DOCTYPE html>
     <html lang="en">
       <head>
-        <meta charset="UTF-8" />
-        <link rel="icon" href="${logoUrl}" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content="${desc}" />
+      <meta charset="UTF-8" />
+      <link rel="icon" href="/favicon.ico" />
+      <meta property="og:locale" content="en_US" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="${title}" />
+      <meta property="og:url" content="${import.meta.env.VITE_APP_URI}" />
+      <meta property="og:site_name" content="${title}" />
+      
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+      <link rel="android-chrome" sizes="256x256" href="/android-chrome-256x256.png">
+      <link rel="android-chrome" sizes="192x192" href="/android-chrome-192x192.png">
+      <meta name="viewport" content="width=device-width">
+      <meta name="description" content="${desc}" />
+      <meta rel="icon" href="/favicon.ico" />
         <title>${title}</title>
       </head>
       <body>
